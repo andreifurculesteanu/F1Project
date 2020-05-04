0aaf6996ac59fecbb905f58c968df8c51b019102
andreifurculesteanu <andrei.furcu@gmail.com>
2020-05-04 13:55:58 +0200

    Information about races added

--- InfoRaces.py
+++ InfoRaces.py
@@ -7,11 +7,12 @@ if(year.isdigit()): #IMPROVE: change isdigit with cast to int and catch exceptio
     if(raceNumber.isdigit()):
         r = requests.get("http://ergast.com/api/f1/{}/{}/results.json".format(year, raceNumber))
         resultsJson = r.json()
-        print((resultsJson))
-        total = resultsJson['MRData']['total']
-        print(total)
         if(int(resultsJson['MRData']['total']) != 0):
-            
+            arrayRaces = resultsJson['MRData']['RaceTable']['Races']
+            print(arrayRaces[0]['raceName'])
+            arrayResults = arrayRaces[0]['Results']
+            for result in arrayResults:
+                print('{}. {} --- Car: {}'.format(result['position'], result['Driver']['familyName'], result['Constructor']['name']))
         else:
             print("The race you are looking for doesn't exist")
     else:
