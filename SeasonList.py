40388b9146808f56a3148d72f488b06c9bbc06eb
andreifurculesteanu <andrei.furcu@gmail.com>
2020-05-12 12:11:58 +0200

    info about every race done

--- SeasonList.py
+++ SeasonList.py
@@ -6,8 +6,12 @@ try:
     intYear = int(year)
     r = requests.get("http://ergast.com/api/f1/{}.json".format(intYear))
     resultsJson = r.json()
+    print(resultsJson)
     if(int(resultsJson['MRData']['total']) != 0):
-        
+        arrayRaces = resultsJson['MRData']['RaceTable']['Races']
+        print("Year: {}".format((intYear)))
+        for races in arrayRaces:
+            print("Round: {} --- Race: {} --- Date: {} --- Circuit: {} --- Locality: {} --- Country: {}".format(races['round'], races['raceName'], races['date'], races['Circuit']['circuitName'], races['Circuit']['Location']['locality'], races['Circuit']['Location']['country']))
     else:
         print("There are no results for the year you are looking")
 
