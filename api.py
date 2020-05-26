9c9cf4d9de36eebadb07ee7d9bc31b904aaf2fb0
andreifurculesteanu <andrei.furcu@gmail.com>
2020-05-26 13:18:10 +0200

    Feature info-constructor

--- api.py
+++ api.py
@@ -1,3 +1,15 @@
-# Get races by year
-# def get_races(year):
-#
+import requests
+
+# Uncomment line to replace " " with "_"
+# Change var "name" with "new_name" in the request
+def get_info_constructor(name):
+    #new_name = name.replace(" ", "_")
+    r = requests.get("http://ergast.com/api/f1/constructors/{}.json".format(name))
+    results_json = r.json()
+    print(results_json)
+    if (int(results_json['MRData']['total']) != 0):
+        url = results_json['MRData']['ConstructorTable']['Constructors'][0]['url']
+    else:
+        url = "The team doesn't exist"
+    return url
+
---
+++ info_constructor.py
@@ -0,0 +1,5 @@
+from api import get_info_constructor
+
+name = input("Constructor name?")
+api = get_info_constructor(name)
+print(api)
