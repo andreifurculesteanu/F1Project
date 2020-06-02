b9350322a9419f8c9631aa895397d23fc0c1ac08
andreifurculesteanu <andrei.furcu@gmail.com>
2020-06-02 12:44:59 +0200

    input string sanitazed

--- api.py
+++ api.py
@@ -3,7 +3,8 @@ import requests
 # Uncomment line to replace " " with "_"
 # Change var "name" with "new_name" in the request
 def get_info_constructor(name):
-    #new_name = name.replace(" ", "_")
+    name = name.strip()
+    name = name.replace(" ", "_")
     r = requests.get("http://ergast.com/api/f1/constructors/{}.json".format(name))
     results_json = r.json()
     print(results_json)
--- info_constructor.py
+++ info_constructor.py
@@ -2,4 +2,5 @@ from api import get_info_constructor
 
 name = input("Constructor name?")
 api = get_info_constructor(name)
+print(name)
 print(api)
