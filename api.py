da7b638bd7cda7b0d8b1b5ba9dd23eb70d0c3fc9
andreifurculesteanu <andrei.furcu@gmail.com>
2020-06-08 15:13:37 +0200

    class Api created and method get_info_constructor added

--- api.py
+++ api.py
@@ -1,16 +1,35 @@
 import requests
 
-# Uncomment line to replace " " with "_"
-# Change var "name" with "new_name" in the request
-def get_info_constructor(name):
-    name = name.strip()
-    name = name.replace(" ", "_")
-    r = requests.get("http://ergast.com/api/f1/constructors/{}.json".format(name))
-    results_json = r.json()
-    print(results_json)
-    if (int(results_json['MRData']['total']) != 0):
-        return results_json['MRData']['ConstructorTable']['Constructors'][0]['url']
+class Api:
 
-    return "The team doesn't exist"
+    base_url = "http://ergast.com/{}"
+
+    # Starts with _ because it's private
+    def _make_request(self, url):
+        request = requests.get(self.base_url.format(url))
+        return request.json()
+
+    # URL has to come with {} to add constructor name
+    def get_info_constructor(self, url, name):
+        name = name.strip()
+        name = name.replace(" ", "_")
+        url = url.format(name)
+        results_json = self._make_request(url)
+        print(results_json)
+        if (int(results_json['MRData']['total']) != 0):
+            return results_json['MRData']['ConstructorTable']['Constructors'][0]['url']
+        return "The team doesn't exist"
+
+    # Uncomment line to replace " " with "_"
+    # Change var "name" with "new_name" in the request
+    # def get_info_constructor(self, name):
+    #     name = name.strip()
+    #     name = name.replace(" ", "_")
+    #     r = requests.get("http://ergast.com/api/f1/constructors/{}.json".format(name))
+    #     results_json = r.json()
+    #     print(results_json)
+    #     if (int(results_json['MRData']['total']) != 0):
+    #         return results_json['MRData']['ConstructorTable']['Constructors'][0]['url']
+    #     return "The team doesn't exist"
 
 
