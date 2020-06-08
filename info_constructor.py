6af71878d9be4fafdf3c3e5b490b3f68020a3973
andreifurculesteanu <andrei.furcu@gmail.com>
2020-06-08 15:13:37 +0200

    class Api created and method get_info_constructor added

--- info_constructor.py
+++ info_constructor.py
@@ -1,5 +1,9 @@
-from api import get_info_constructor
+from api import Api
 
 name = input("Constructor name?")
-api = get_info_constructor(name)
-print(api)
+# api = get_info_constructor(name)
+# print(api)
+
+url = "api/f1/constructors/{}.json"
+api = Api()
+print(api.get_info_constructor(url, name))
