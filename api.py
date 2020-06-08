8c785a04e440e4edc9fc741a739b044d9ecee765
andreifurculesteanu <andrei.furcu@gmail.com>
2020-06-08 16:44:18 +0200

    -url as param, added in method

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
-class Api:
 
+class Api:
     base_url = "http://ergast.com/{}"
 
     # Starts with _ because it's private
@@ -9,27 +9,11 @@ class Api:
         request = requests.get(self.base_url.format(url))
         return request.json()
 
-    # URL has to come with {} to add constructor name
-    def get_info_constructor(self, url, name):
+    def get_info_constructor(self, name):
         name = name.strip()
         name = name.replace(" ", "_")
-        url = url.format(name)
-        results_json = self._make_request(url)
+        results_json = self._make_request("api/f1/constructors/{}.json".format(name))
         print(results_json)
-        if (int(results_json['MRData']['total']) != 0):
+        if int(results_json['MRData']['total']) != 0:
             return results_json['MRData']['ConstructorTable']['Constructors'][0]['url']
         return "The team doesn't exist"
-
-    # Uncomment line to replace " " with "_"
-    # Change var "name" with "new_name" in the request
-    # def get_info_constructor(self, name):
-    #     name = name.strip()
-    #     name = name.replace(" ", "_")
-    #     r = requests.get("http://ergast.com/api/f1/constructors/{}.json".format(name))
-    #     results_json = r.json()
-    #     print(results_json)
-    #     if (int(results_json['MRData']['total']) != 0):
-    #         return results_json['MRData']['ConstructorTable']['Constructors'][0]['url']
-    #     return "The team doesn't exist"
-
-
--- info_constructor.py
+++ info_constructor.py
@@ -1,9 +1,6 @@
 from api import Api
 
 name = input("Constructor name?")
-# api = get_info_constructor(name)
-# print(api)
 
-url = "api/f1/constructors/{}.json"
 api = Api()
-print(api.get_info_constructor(url, name))
+print(api.get_info_constructor(name))
