2837737b3ec62ec7b7c525be66d776fbac557576
andreifurculesteanu <andrei.furcu@gmail.com>
2020-06-26 16:01:40 +0200

    classes race and circuit created

---
+++ src/models/circuit.py
@@ -0,0 +1,9 @@
+class Circuit:
+    name = ""
+    location = ""
+    country = ""
+
+    def __init__(self, name, location, country):
+        self.name = name
+        self.location = location
+        self.country = country
