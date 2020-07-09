4f77801a2b411a65c18349ddad3d5f9fa04191dd
andreifurculesteanu <andrei.furcu@gmail.com>
2020-07-09 14:43:36 +0200

    more things

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/paco')
+def paco():
+    return 'My name is Paco'
+
+
+@app.route('/param')
+def param():
+    params = request.args.get('my_param', 'nothing')
+    return 'Param is = {}'.format(params)
+
+
+app.run(debug=True)
