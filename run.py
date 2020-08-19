84e5bdf7a84f51c9956e8257b0dec0e9283ba13b
andreifurculesteanu <andrei.furcu@gmail.com>
2020-08-19 15:15:07 +0200

    moving to ubuntu

---
+++ run.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+app.run(debug=True)
