b44bbba10753f3653138e63afe45db875a91a2c4
andreifurculesteanu <andrei.furcu@gmail.com>
2020-08-25 11:15:56 +0200

    user model created

---
+++ src/models/user.py
@@ -0,0 +1,11 @@
+class User:
+    username = ""
+    email = ""
+    password = ""
+    date = ""
+
+    def __init__(self, username, email, password, date):
+        self.username = username
+        self. email = email
+        self.password = password
+        self.date = date
