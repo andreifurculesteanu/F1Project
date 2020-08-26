f7afebd41f7295d4759d557395818fb25d38f512
andreifurculesteanu <andrei.furcu@gmail.com>
2020-08-26 14:31:37 +0200

    routes changed

--- run.py
+++ run.py
@@ -1,29 +1,5 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from src import app
 
 if __name__ == '__main__':
-    app.config.from_pyfile('./config/config.cfg')
-    app.run()
+    app.run(host='0.0.0.0')
 
